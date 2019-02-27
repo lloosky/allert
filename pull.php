80814e43155bebdda2e37ee120d9ce6f42e20836
Pawel Switalla <swit3k@gmail.com>
2019-02-27 23:27:10 +0100

    pull.php update

--- pull.php
+++ pull.php
@@ -1,5 +1,5 @@
 <?php
-$dump = var_dump($_POST);
+$dump = print_r($_POST, true);
 $log = $dump.PHP_EOL;
-file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
+file_put_contents('./log_'.date("j-n-Y").'.log', $log, FILE_APPEND);
 ?>
