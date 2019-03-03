cd45f3a9456f1a09199fd247432e334b9e3b3fe0
Pawel Switalla <swit3k@gmail.com>
2019-03-03 12:57:53 +0100

    test commit #3

--- pull.php
+++ pull.php
@@ -2,6 +2,6 @@
 #$dump = print_r($_POST, true);
 #$log = $dump.PHP_EOL;
 #file_put_contents('./log_'.date("j-n-Y").'.log', $log, FILE_APPEND);
-exec('git pull -q origin master');
+file_put_contents('./php.log', exec('git pull -q origin master').PHP_EOL, FILE_APPEND);
 ?>
 
